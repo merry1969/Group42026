f29f6f0bbd1c890a60751bc4adcd424df5463163
MerryMashila <merry19729@gmail.com>
2026-07-20 19:45:51 +0200

    amended logInToNdosiMultipleMethod

--- src/test/java/basic/logInToNdosiMultipleMethod.java
+++ src/test/java/basic/logInToNdosiMultipleMethod.java
@@ -1,4 +1,5 @@
-import net.bytebuddy.implementation.bytecode.Throw;
+package basic;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,183 +22,184 @@ public class logInToNdosiMultipleMethod {
 
     }
 
-    @Test
+    @Test(priority = 1)
     public void clickLogin(){
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[1]")).click();
 
+
+
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void enterEmail() throws InterruptedException {
         Thread.sleep(4000);
          driver.findElement(By.id("login-email")).sendKeys("merry1234@gmail.com");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void enterPassword() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.id("login-password")).sendKeys("junmafu@123");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void clickLoginButton() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.id("login-submit")).click();
 
     }
 
-    @Test(priority = 4)
+    @Test(priority = 5)
     public void verifyLogin() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/div/button/span[1]")).isDisplayed();
     }
 
-    @Test(priority = 5)
+    @Test(priority = 6)
     public void selectLearnDropdown() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/button/span[2]")).click();
 
     }
 
-    @Test(priority = 6)
+    @Test(priority = 7)
     public void selectLearningMaterials() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/div/button[2]/span[2]")).click();
 
     }
 
-    @Test(priority = 7)
+    @Test(priority = 8)
     public void validateLearningMaterialsPage() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"practice-section\"]/p")).isDisplayed();
 
     }
-    @Test(dependsOnMethods = "validateLearningMaterialsPage")
+    @Test(priority = 9)
     public void selectWebAutomationBasicForm() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"tab-btn-password\"]/span[2]")).click();
     }
 
-    @Test(dependsOnMethods = "selectWebAutomationBasicForm")
+    @Test(priority = 10)
     public void validateBasicPracticeFrom() throws InterruptedException {
         Thread.sleep(4000);
-        driver.findElement(By.xpath("page-title")).isDisplayed();
+        driver.findElement(By.id("page-title")).isDisplayed();
 
     }
 
-    @Test(dependsOnMethods = "validateBasicPracticeFrom")
+    @Test(priority = 11)
     public void fillInFullName() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.id("name")).sendKeys("merry mashila");
 
     }
 
-    @Test(dependsOnMethods ="fillInFullName")
+    @Test(priority = 12)
     public void fillInEmailAddress() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.id("email")).sendKeys("merry1234@gmail.com");
     }
 
-    @Test(dependsOnMethods = "fillInEmailAddress" )
+    @Test(priority = 13)
     public void fillInAge() throws InterruptedException {
         Thread.sleep(4000);
-        driver.findElement(By.id("age")).sendKeys("16");
+        driver.findElement(By.id("age")).sendKeys("25");
     }
 
-    @Test(dependsOnMethods = "fillInAge" )
+    @Test(priority = 14)
     public void selectGenderButton() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("gender")).click();
+        driver.findElement(By.id("gender")).sendKeys("male");
     }
 
-    @Test(dependsOnMethods = "selectGenderButton")
+    @Test(priority = 15)
     public void selectCountry() throws InterruptedException {
-        Thread.sleep(4000);
-        driver.findElement(By.id("//country")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("country")).click();
+        driver.findElement(By.id("country")).sendKeys("south africa");
     }
 
-    @Test(dependsOnMethods = "selecctCountryButton")
+    @Test(priority = 16)
     public void selectExperience() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("experience")).click();
+        driver.findElement(By.id("experience")).sendKeys("Intermediate (2-5 years");
     }
 
-    @Test(dependsOnMethods = "selectExperienceButton")
-    public void selectJaveScript() throws InterruptedException {
-        Thread.sleep(4000);
+    @Test(priority = 17)
+    public void selectJavaScript() throws InterruptedException {
+        Thread.sleep(3000);
         driver.findElement(By.id("skill-javascript")).click();
     }
 
-    @Test(dependsOnMethods = "selectJavaScript" )
+    @Test(priority = 18)
     public void selectSelenium() throws InterruptedException {
-    Thread.sleep(4000);
+    Thread.sleep(3000);
     driver.findElement(By.id("skill-selenium")).click();
 
     }
 
     @Test(priority = 19)
     public void addAdditionalComment() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("comments")).sendKeys("This is my web basic automation form");
 
     }
     @Test(priority = 20)
     public void clickSubscribeToNewsLetter() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("newsletter")).click();
 
     }
 
     @Test(priority = 21)
     public void clickTermsAndConditions() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("terms")).click();
 
     }
 
     @Test(priority = 22)
     public void clickValidate() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("validate-btn")).click();
     }
 
     @Test( priority = 23)
     public void confirmValidationPopUp() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"practice-section\"]/div[3]/div[1]")).isDisplayed();
     }
 
     @Test(priority= 24 )
     public void selectSubmitForm() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("submit-btn")).click();
     }
 
     @Test( priority = 25)
     public void confirmSubmitFormPopUp() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"practice-section\"]/div[3]/div[1]")).isDisplayed();
     }
 
     @Test(priority = 26)
     public void viewSubmissions() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("submissions-toggle-btn")).click();
     }
-    @Test(priority = 27)
-    public void viewForm () throws InterruptedException {
-        Thread.sleep(4000);
-        driver.findElement(By.id("view-submission-0")).click();
-    }
-    @Test (priority = 28)
+
+    @Test (priority = 27)
     public void viewFormData() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"practice-form\"]/details/summary")).click();
     }
 
-    @Test (priority = 29)
+    @Test (priority = 28)
     public void viewFormDataInfo() throws InterruptedException {
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         driver.findElement(By.id("form-data")).isDisplayed();
     }
 
